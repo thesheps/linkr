417b5c20af831a8453259c1dbae12e2d78d9bfc3
Sheps <thesheps@gmail.com>
2022-06-11 17:46:51 +0100

    feat: adds route53 and certificate shenanigans

--- infra/linkr-stack.ts
+++ infra/linkr-stack.ts
@@ -4,40 +4,62 @@ import { Function, Code, Runtime } from "aws-cdk-lib/aws-lambda";
 import { LambdaIntegration, Method, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { HttpMethod } from "aws-cdk-lib/aws-events";
 import { Construct } from "constructs";
+import {
+	ARecord,
+	PublicHostedZone,
+	RecordTarget,
+} from "aws-cdk-lib/aws-route53";
+import { ApiGateway } from "aws-cdk-lib/aws-route53-targets";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 
 export class LinkrStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
 
-    const table = new Table(this, "Proxy", {
-      partitionKey: { name: "path", type: AttributeType.STRING },
-      tableName: "linkr-entries",
-    });
+		const table = new Table(this, "Proxy", {
+			partitionKey: { name: "path", type: AttributeType.STRING },
+			tableName: "linkr-entries",
+		});
 
-    const dynamoLambda = new Function(this, "DynamoLambdaHandler", {
-      runtime: Runtime.NODEJS_12_X,
-      code: Code.fromAsset("build"),
-      handler: "lambda.handler",
-      functionName: "linkr-handler",
-      environment: {
-        PROXY_TABLE_NAME: table.tableName,
-      },
-    });
+		const dynamoLambda = new Function(this, "DynamoLambdaHandler", {
+			runtime: Runtime.NODEJS_12_X,
+			code: Code.fromAsset("build"),
+			handler: "lambda.handler",
+			functionName: "linkr-handler",
+			environment: {
+				PROXY_TABLE_NAME: table.tableName,
+			},
+		});
 
-    table.grantReadWriteData(dynamoLambda);
+		table.grantReadWriteData(dynamoLambda);
 
-    const api = new RestApi(this, "ProxyApi", {
-      defaultIntegration: new LambdaIntegration(dynamoLambda),
-      restApiName: "linkr-proxy-api",
-    });
+		const recordName = process.env.LINKR_DOMAIN ?? "";
+		const certificate = new Certificate(this, "ProxyApiCertificate", {
+			domainName: recordName,
+		});
 
-    const getMethod = new Method(this, "GetMethod", {
-      httpMethod: HttpMethod.GET,
-      resource: api.root,
-    });
+		const api = new RestApi(this, "ProxyApi", {
+			domainName: { certificate, domainName: recordName },
+			defaultIntegration: new LambdaIntegration(dynamoLambda),
+			restApiName: "linkr-proxy-api",
+		});
 
-    new CfnOutput(this, "HTTP API Url", {
-      value: api.url ?? "API URL not found",
-    });
-  }
+		const getMethod = new Method(this, "GetMethod", {
+			httpMethod: HttpMethod.GET,
+			resource: api.root,
+		});
+
+		const zone = new PublicHostedZone(this, "LinkrHostedZone", {
+			zoneName: "linkr",
+		});
+
+		new ARecord(this, "LinkrRecord", {
+			zone,
+			target: RecordTarget.fromAlias(new ApiGateway(api)),
+		});
+
+		new CfnOutput(this, "HTTP API Url", {
+			value: api.url ?? "API URL not found",
+		});
+	}
 }
--- infra/test/linkr.test.ts
+++ infra/test/linkr.test.ts
@@ -1,66 +1,88 @@
-import { App } from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { App } from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
 
-import { LinkrStack } from '../linkr-stack';
+import { LinkrStack } from "../linkr-stack";
 
-const stack = new LinkrStack(new App(), 'MyTestStack');
+process.env.LINKR_DOMAIN = "linkr.com";
+const stack = new LinkrStack(new App(), "MyTestStack");
 const template = Template.fromStack(stack);
 
-test('DynamoDB', () => {
-  template.hasResourceProperties("AWS::DynamoDB::Table", {
-    KeySchema: [
-      {
-        AttributeName: "path",
-        KeyType: "HASH"
-      }
-    ],
-    AttributeDefinitions: [
-      {
-        AttributeName: "path",
-        AttributeType: "S"
-      }
-    ],
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 5,
-      WriteCapacityUnits: 5
-    }
-  });
+test("DynamoDB", () => {
+	template.hasResourceProperties("AWS::DynamoDB::Table", {
+		KeySchema: [
+			{
+				AttributeName: "path",
+				KeyType: "HASH",
+			},
+		],
+		AttributeDefinitions: [
+			{
+				AttributeName: "path",
+				AttributeType: "S",
+			},
+		],
+		ProvisionedThroughput: {
+			ReadCapacityUnits: 5,
+			WriteCapacityUnits: 5,
+		},
+	});
 });
 
-test('DynamoDB Read/Write IAM Policy', () => {
-  template.hasResourceProperties("AWS::IAM::Policy", {
-    PolicyDocument: {
-      Statement: [
-        {
-          Action: [
-            "dynamodb:BatchGetItem",
-            "dynamodb:GetRecords",
-            "dynamodb:GetShardIterator",
-            "dynamodb:Query",
-            "dynamodb:GetItem",
-            "dynamodb:Scan",
-            "dynamodb:ConditionCheckItem",
-            "dynamodb:BatchWriteItem",
-            "dynamodb:PutItem",
-            "dynamodb:UpdateItem",
-            "dynamodb:DeleteItem",
-            "dynamodb:DescribeTable",
-          ],
-          Effect: "Allow"
-        }]
-    }
-  });
+test("DynamoDB Read/Write IAM Policy", () => {
+	template.hasResourceProperties("AWS::IAM::Policy", {
+		PolicyDocument: {
+			Statement: [
+				{
+					Action: [
+						"dynamodb:BatchGetItem",
+						"dynamodb:GetRecords",
+						"dynamodb:GetShardIterator",
+						"dynamodb:Query",
+						"dynamodb:GetItem",
+						"dynamodb:Scan",
+						"dynamodb:ConditionCheckItem",
+						"dynamodb:BatchWriteItem",
+						"dynamodb:PutItem",
+						"dynamodb:UpdateItem",
+						"dynamodb:DeleteItem",
+						"dynamodb:DescribeTable",
+					],
+					Effect: "Allow",
+				},
+			],
+		},
+	});
 });
 
-test('Lambda', () => {
-  template.hasResourceProperties("AWS::Lambda::Function", {
-    Handler: "lambda.handler",
-    Runtime: "nodejs12.x"
-  });
+test("Lambda", () => {
+	template.hasResourceProperties("AWS::Lambda::Function", {
+		Handler: "lambda.handler",
+		Runtime: "nodejs12.x",
+	});
 });
 
-describe('API Gateway', () => {
-  template.hasResourceProperties("AWS::ApiGateway::RestApi", {
-    Name: "linkr-proxy-api",
-  });
+test("API Gateway", () => {
+	template.hasResourceProperties("AWS::ApiGateway::RestApi", {
+		Name: "linkr-proxy-api",
+	});
+});
+
+test("Certificate", () => {
+	template.hasResourceProperties("AWS::CertificateManager::Certificate", {
+		DomainName: "linkr.com",
+		DomainValidationOptions: [
+			{
+				DomainName: "linkr.com",
+				ValidationDomain: "linkr.com",
+			},
+		],
+		ValidationMethod: "EMAIL",
+	});
+});
+
+test("Route53", () => {
+	template.hasResourceProperties("AWS::Route53::RecordSet", {
+		Name: "linkr.",
+		AliasTarget: {},
+	});
 });
