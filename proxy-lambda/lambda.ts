763579c8d56f619c7fedd9eb4b4402304e542638
Sheps <thesheps@gmail.com>
2022-06-12 09:31:34 +0100

    feat: redirects the user back to the homepage

--- proxy-lambda/lambda.ts
+++ proxy-lambda/lambda.ts
@@ -4,21 +4,25 @@ exports.handler = async function (event: any) {
 	console.log("request:", JSON.stringify(event, undefined, 2));
 
 	const TableName = process.env.PROXY_TABLE_NAME;
-
 	if (!TableName) return sendResponse(505, "Proxy table not found!");
 
 	const dynamo = new DynamoDB();
 
 	await dynamo
 		.updateItem({
-			TableName: process.env.PROXY_TABLE_NAME,
+			TableName,
 			Key: { path: { S: event.requestContext.path } },
 			UpdateExpression: "ADD hits :incr",
 			ExpressionAttributeValues: { ":incr": { N: "1" } },
 		})
 		.promise();
 
-	return sendResponse(200, "You have connected with the Lambda!");
+	return {
+		statusCode: 301,
+		headers: {
+			Location: "https://www.thesheps.dev",
+		},
+	};
 };
 
 const sendResponse = (status: number, body: string) => {
--- proxy-lambda/test/lambda.test.ts
+++ proxy-lambda/test/lambda.test.ts
@@ -1,7 +1,21 @@
 const lambda = require("../lambda");
 
+jest.mock("aws-sdk");
+
+const testEvent = { requestContext: { path: "/beans" } };
+const updateItem = jest.fn().mockReturnValue({ promise: jest.fn() });
+
 describe("Proxy Lambda", () => {
+	const { DynamoDB } = require("aws-sdk");
+
+	beforeEach(() => {
+		DynamoDB.mockImplementation(() => ({
+			updateItem,
+		}));
+	});
+
 	it("Throws an error when misconfigured", async () => {
+		delete process.env.PROXY_TABLE_NAME;
 		const response = await lambda.handler();
 
 		expect(response).toEqual({
@@ -10,6 +24,30 @@ describe("Proxy Lambda", () => {
 				"Content-Type": "text/html",
 			},
 			statusCode: 505,
+		});
+	});
+
+	it("Updates the hit count for the specified path", async () => {
+		process.env.PROXY_TABLE_NAME = "beans on toast";
+		await lambda.handler(testEvent);
+
+		expect(updateItem).toBeCalledWith({
+			ExpressionAttributeValues: { ":incr": { N: "1" } },
+			Key: { path: { S: "/beans" } },
+			TableName: "beans on toast",
+			UpdateExpression: "ADD hits :incr",
+		});
+	});
+
+	it("Redirects the user to thesheps.dev", async () => {
+		process.env.PROXY_TABLE_NAME = "beans on toast";
+		const response = await lambda.handler(testEvent);
+
+		expect(response).toEqual({
+			headers: {
+				Location: "https://www.thesheps.dev",
+			},
+			statusCode: 301,
 		});
 	});
 });
