71e2f77db6088ef294fda4c93199efcb53a7c81c
Sheps <thesheps@gmail.com>
2022-06-12 12:38:51 +0100

    feat(proxy): introduces default redirect variable

--- proxy-lambda/proxy-lambda.ts
+++ proxy-lambda/proxy-lambda.ts
@@ -3,14 +3,17 @@ const { DynamoDB, Lambda } = require("aws-sdk");
 exports.handler = async function (event: any) {
 	console.log("request:", JSON.stringify(event, undefined, 2));
 
-	const TableName = process.env.PROXY_TABLE_NAME;
-	if (!TableName) return sendResponse(505, "Proxy table not found!");
+	const tableName = process.env.PROXY_TABLE_NAME;
+	const defaultRedirect = process.env.DEFAULT_REDIRECT;
+
+	if (!defaultRedirect) return sendResponse(505, "Default redirect not found!");
+	if (!tableName) return sendResponse(505, "Proxy table not found!");
 
 	const dynamo = new DynamoDB();
 
 	await dynamo
 		.updateItem({
-			TableName,
+			TableName: tableName,
 			Key: { path: { S: event.requestContext.path } },
 			UpdateExpression: "ADD hits :incr",
 			ExpressionAttributeValues: { ":incr": { N: "1" } },
@@ -20,7 +23,7 @@ exports.handler = async function (event: any) {
 	return {
 		statusCode: 301,
 		headers: {
-			Location: "https://www.thesheps.dev",
+			Location: defaultRedirect,
 		},
 	};
 };
--- proxy-lambda/test/proxy-lambda.test.ts
+++ proxy-lambda/test/proxy-lambda.test.ts
@@ -3,6 +3,8 @@ const lambda = require("../proxy-lambda");
 jest.mock("aws-sdk");
 global.console.log = jest.fn();
 
+const proxyTable = "beans-on-toast";
+const defaultRedirect = "default-redirect.com";
 const testEvent = { requestContext: { path: "/beans" } };
 const updateItem = jest.fn().mockReturnValue({ promise: jest.fn() });
 
@@ -10,6 +12,9 @@ describe("Proxy Lambda", () => {
 	const { DynamoDB } = require("aws-sdk");
 
 	beforeEach(() => {
+		process.env.PROXY_TABLE_NAME = proxyTable;
+		process.env.DEFAULT_REDIRECT = defaultRedirect;
+
 		DynamoDB.mockImplementation(() => ({
 			updateItem,
 		}));
@@ -28,25 +33,36 @@ describe("Proxy Lambda", () => {
 		});
 	});
 
+	it("Throws an error when misconfigured", async () => {
+		delete process.env.DEFAULT_REDIRECT;
+		const response = await lambda.handler();
+
+		expect(response).toEqual({
+			body: "Default redirect not found!",
+			headers: {
+				"Content-Type": "text/html",
+			},
+			statusCode: 505,
+		});
+	});
+
 	it("Updates the hit count for the specified path", async () => {
-		process.env.PROXY_TABLE_NAME = "beans on toast";
 		await lambda.handler(testEvent);
 
 		expect(updateItem).toBeCalledWith({
 			ExpressionAttributeValues: { ":incr": { N: "1" } },
 			Key: { path: { S: "/beans" } },
-			TableName: "beans on toast",
+			TableName: proxyTable,
 			UpdateExpression: "ADD hits :incr",
 		});
 	});
 
-	it("Redirects the user to thesheps.dev", async () => {
-		process.env.PROXY_TABLE_NAME = "beans on toast";
+	it("Redirects the user to default redirect", async () => {
 		const response = await lambda.handler(testEvent);
 
 		expect(response).toEqual({
 			headers: {
-				Location: "https://www.thesheps.dev",
+				Location: defaultRedirect,
 			},
 			statusCode: 301,
 		});
