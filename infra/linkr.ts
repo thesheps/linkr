b9f102a91c8d9e32ba618a7f418be5f1200e3d45
Sheps <thesheps@gmail.com>
2022-06-12 14:03:19 +0100

    feat: renames all needed env vars with LINKR

--- infra/linkr.ts
+++ infra/linkr.ts
@@ -5,10 +5,10 @@ import { LinkrStack, LinkrStackProps } from "./linkr-stack";
 const props: LinkrStackProps = {
 	linkrDomainName: process.env.LINKR_DOMAIN ?? "",
 	linkrHostedZoneId: process.env.LINKR_HOSTED_ZONE_ID ?? "",
-	defaultRedirect: process.env.DEFAULT_REDIRECT ?? "",
+	defaultRedirect: process.env.LINKR_DEFAULT_REDIRECT ?? "",
 };
 
-if (!Object.values(props).every((p) => p))
+if (!Object.values(props).every((p) => !!p))
 	throw new Error("Environment not configured!");
 
 new LinkrStack(new App(), "Linkr", {
