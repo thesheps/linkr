8111b0580417d8a5b81433e2785026351176dee0
Sheps <thesheps@gmail.com>
2022-06-13 18:06:37 +0100

    test: updates smoke tests

--- scripts/smoke-tests.sh
+++ scripts/smoke-tests.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
+# Test constants
+SHORT_URL="https://sheps.link/1z6bGk"
+
+# Import smoke.sh library
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 . "$SCRIPT_DIR/smoke.sh"
 
-# Proxy url
-smoke_url_ok "https://$LINKR_DOMAIN"
-
-# Admin url
+# Smoke admin api
 API_KEY=$(aws secretsmanager get-secret-value --secret-id linkr-api-key | jq ".SecretString | fromjson | .key" -r)
 smoke_header "x-api-key: $API_KEY"
 smoke_form_ok "https://api.$LINKR_DOMAIN/entries" "$SCRIPT_DIR/test-url"
-smoke_assert_body '{"shortUrl":"https://sheps.link/1z6bGk"}'
+smoke_assert_body "{\"shortUrl\":\"$SHORT_URL\"}"
+
+# Smoke proxy api
+smoke_url_ok "https://$LINKR_DOMAIN"
+smoke_url_ok "$SHORT_URL"
